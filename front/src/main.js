84a6e002c29ae0a885e0be13febe104c04b43dad
mogya <mogya@mogya.com>
2017-11-04 20:56:50 +0900

    IEでうまく動作しなかった問題を修正

--- front/src/main.js
+++ front/src/main.js
@@ -2,13 +2,9 @@ import Vue from 'vue';
 import VueRouter from 'vue-router'
 import MapView from './map.vue';
 import ContributeView from './contribute.vue';
-import Promise from 'promise-polyfill';
 import './assets/bootstrap/css/bootstrap.min.css';
 import './assets/fontello/css/fontello.css';
 
-if (!window.Promise) {
-  window.Promise = Promise;
-}
 
 Vue.use(VueRouter);
 const routes = [
