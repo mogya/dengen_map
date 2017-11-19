28e22a59fdf2fcdddf3bee0fd470d716bd14fde3
mogya <mogya@mogya.com>
2017-11-19 13:04:31 +0900

    カテゴリとアイコンを出せるようにする #27 カテゴリデータをキャッシュするようにしてパフォーマンスを改善した

--- app/models/tag/category.rb
+++ app/models/tag/category.rb
@@ -1,10 +1,12 @@
 class Tag::Category < Tag
   IMAGE_PREFIX = '//oasis.mogya.com/images/design/'
+  class_attribute :categories_cache_data
 
   def self.prime_category(categories, with_image=false)
     candidate_category = nil
     categories.each do |category|
-      tag = Tag::Category.find_by(name:category)
+      # tag = Tag::Category.find_by(name:category)
+      tag = categories_cache[category]
       next if (with_image && tag.image.nil?)
       if candidate_category.nil? || (candidate_category.importance <= tag.importance)
         candidate_category = tag
@@ -15,5 +17,19 @@ class Tag::Category < Tag
 
   def image_path
     "#{IMAGE_PREFIX}#{image}" if image
+  end
+
+
+  private
+  # todo とっても読みづらいし、いつキャッシュが更新されるのか怪しい(セッションごとじゃないっぽい？)ので、変更時に反映されないおそれがある。
+  # spot側に演算済みカテゴリのキャッシュを持つようにした方がいいかも。
+  def self.categories_cache
+    if Tag::Category.categories_cache_data.nil?
+      Tag::Category.categories_cache_data = {}
+      Tag::Category.all.each do |category|
+        Tag::Category.categories_cache_data[category.name] = category
+      end
+    end
+    Tag::Category.categories_cache_data
   end
 end
