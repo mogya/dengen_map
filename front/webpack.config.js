83c10d6690cce2093a8fb687d651d4799bebca6e
daisuke furukawa <mogya@mogya.com>
2019-07-21 19:28:24 +0900

    babel設定を統合

--- front/webpack.config.js
+++ front/webpack.config.js
@@ -19,9 +19,6 @@ module.exports = {
         loader: 'vue-loader',
         options: {
           loaders: {
-            // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
-            // the "scss" and "sass" values for the lang attribute to the right configs here.
-            // other preprocessors should work out of the box, no loader config like this nessessary.
             scss: 'vue-style-loader!css-loader!sass-loader',
             sass: 'vue-style-loader!css-loader!sass-loader?indentedSyntax'
           }
@@ -31,7 +28,10 @@ module.exports = {
       {
         test: /\.js$/,
         loader: 'babel-loader',
-        exclude: /node_modules/
+        exclude: /node_modules/,
+        options: {
+          presets: ['@babel/preset-env']
+        }
       },
       {
         test: /\.(png|jpg|gif|svg)$/,
